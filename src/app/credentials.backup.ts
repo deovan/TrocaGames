50604ca11537ae8c3acc10477d0095b24f5b383c
Deovan <dedezanol9@gmail.com>
2018-06-24 14:38:33 -0300

    gitignore

--- src/app/credentials.backup.ts
+++ src/app/credentials.backup.ts
@@ -1,8 +1,18 @@
 
 export const firebaseConfig = {
-
+  apiKey: "AIzaSyBstE_okp65Iwg6efdHZrKNE8QVbMmml6k",
+  authDomain: "trocagames-12485.firebaseapp.com",
+  databaseURL: "https://trocagames-12485.firebaseio.com",
+  projectId: "trocagames-12485",
+  storageBucket: "trocagames-12485.appspot.com",
+  messagingSenderId: "917983614161"
 }
 
 export const firebaseConfigTesting = {
-
+  apiKey: "AIzaSyClEwiBlbSqscYpkDYeidLAIPGf_DXsGGw",
+  authDomain: "trocagamestesting.firebaseapp.com",
+  databaseURL: "https://trocagamestesting.firebaseio.com",
+  projectId: "trocagamestesting",
+  storageBucket: "trocagamestesting.appspot.com",
+  messagingSenderId: "1082426681874"
 }
